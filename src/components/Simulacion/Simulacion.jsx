7ab23e976bdc4f0288c2772fdfdf7f934521b072
luismagnum <luismagnum1@gmail.com>
2025-03-25 01:04:35 -0300

    first commit

--- src/components/Simulacion/Simulacion.jsx
+++ src/components/Simulacion/Simulacion.jsx
@@ -13,7 +13,7 @@ export default function Simulacion() {
     <section className='mt-0 mx-4'>
   <div className='grid place-items-center justify-center gap-1 pt-0'>
     <h2 className='text-center text-cyan-400 text-3xl font-bold py-2 mt-24'>
-      Simulacion de proyecto 
+      Simulacion de Proyecto
     </h2>
   </div>
   <div className='flex text-center justify-center mx-auto p-16 mt-1'>
